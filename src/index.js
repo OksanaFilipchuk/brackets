98d003b3073fd37d8a0594e91944ac1e9349a08b
OksanaFilipchuk <filipchuk.oksana@gmail.com>
2022-07-31 19:17:12 +0300

    implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  let obj = {};
+  bracketsConfig.forEach(elem =>{
+  obj[elem[0]] = elem[1]
+})
+
+  let stack = [];
+  [...str].forEach((elemStr,index) => {
+    
+      let isOpen = false;
+    bracketsConfig.forEach(elem => {
+      if (elemStr ===elem[0]){
+        isOpen = isOpen || true;
+      }     
+    })
+    let isClose = false;
+    bracketsConfig.forEach(elem => {
+      if (elemStr === elem[1]){
+        isClose = isClose || true;
+      }     
+    })
+
+    if(isOpen && !isClose){
+      stack.push(elemStr);      
+    } else 
+    if((isClose && !isOpen && stack.length!=0)){        
+        let stackLastElem = stack[stack.length-1];
+        if(obj[stackLastElem] === elemStr){
+          stack.pop() 
+        }               
+    }else
+    if(isOpen && isClose){          
+          let stackLastElem = stack[stack.length-1];          
+          if(obj[stackLastElem] === elemStr){
+          stack.pop();
+          } else {
+            stack.push(elemStr);
+          }     
+      
+    } else {
+    stack.push(elemStr);
+      } 
+    
+  })
+return stack.length === 0
 }
